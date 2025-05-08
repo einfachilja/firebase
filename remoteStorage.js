d3b236bb6a619d8bf8707891e71b4cc00ab2f72f
Ilja Savkiv <iljasavkiv@MacBookPro.fritz.box>
2025-05-08 09:52:23 +0200

    added path to loadData()

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,12 +1,12 @@
 function onloadFunc() {
     console.log('test');
-    loadData();
+    loadData('/name'); // pfad für untere struktur
 }
 
 const BASE_URL = 'https://remotestorage-82664-default-rtdb.europe-west1.firebasedatabase.app/'; // konstant von Anfang an definieren
 
-async function loadData() {
-    let response = await fetch(BASE_URL + '.json') // am Ende der url nach .json fragen, sonst gehts nicht!
-    let responseJson = await response.json();
-    console.log(responseJson);
+async function loadData(path='') { // zusätzlicher pfad um auf z.B. name zuzugreifen
+    let response = await fetch(BASE_URL + path + '.json') // am Ende der url nach .json fragen, sonst gehts nicht!
+    let responseJson = await response.json(); // wenn keine methode definiert, dann standard GET
+    console.log(responseJson); // ausloggen in console oder returnen und mit loadData() weiterarbeiten...
 }
